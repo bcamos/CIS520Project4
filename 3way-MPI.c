e0e6086e95fa4c900c096771c8a583de8383cd1b
Ben Amos <bcamos@ksu.edu>
2021-05-03 14:29:05 -0500

    Current status on opneMPI using scatter

--- 3way-MPI.c
+++ 3way-MPI.c
@@ -3,19 +3,22 @@
 #include <stdio.h>
 #include <string.h>
 
-#define BATCH_SIZE 1000000
+#define MAX_SIZE 1000000
 #define MAX_STRING 2001
-char lines[BATCH_SIZE];
-float global_averages[BATCH_SIZE];
-float local_averages[BATCH_SIZE];
+char* lines[MAX_SIZE];
+char** local_lines;
+float* global_averages;
+float* local_averages;
 int NUM_THREADS;
+int BATCH_SIZE;
 
 float find_avg(char* line, int nchars) 
 {
    int i, j;
    float sum = 0;
 
-   for ( i = 0; i < nchars; i++ ) {
+   for ( i = 0; i < nchars; i++ ) 
+   {
       sum += ((int) line[i]);
    }
 
@@ -36,23 +39,33 @@ void calculate_averages(void *rank, int lastIdx)
     int myId = *((int*) rank);
     int startPos = ((long)myId) * (BATCH_SIZE / NUM_THREADS);
     int endPos = startPos + (BATCH_SIZE / NUM_THREADS);
-    if (endPos > lastIdx)
+    if (endPos - startPos < BATCH_SIZE)
     {
-        endPos = lastIdx;
+        endPos -= startPos;
+    }
+    else
+    {
+        endPos = BATCH_SIZE;
     }
 
     printf("myID = %d startPos = %d endPos = %d \n", myId, startPos, endPos); fflush(stdout);
 
-    for (i = 0; i < lastIdx; i++)
+    for (i = 0; i < BATCH_SIZE; i++)
     {
-        local_averages[i] = 0.0;
+        local_averages[i] = 0.0f;
     }
 
-    for (i = startPos; i < endPos; i++)
+    printf("myID = %d Init success \n", myId); fflush(stdout);
+
+    if (myId == 0)
     {
-        strLength = strlen(lines[i]);
-        local_averages[i] = find_avg(lines[i], strLength);
-    }
+        for (i = 0; i < endPos; i++)
+        {
+            strLength = strlen(local_lines[i]);
+            local_averages[i] = find_avg(local_lines[i], strLength);
+        }
+    }    
+    printf("myID = %d Done! \n", myId); fflush(stdout);
 }
 
 int main(int argc, char* argv[])
@@ -76,15 +89,17 @@ int main(int argc, char* argv[])
    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
    
    NUM_THREADS = numtasks;
+   BATCH_SIZE = MAX_SIZE / NUM_THREADS;
    printf("size = %d rank = %d\n", numtasks, rank);
    fflush(stdout);
-
+   int* localLineCount = malloc(sizeof(int));
    if (rank == 0)
    {
+       global_averages = malloc(sizeof(float) * MAX_SIZE);
        fd = fopen("/homes/dan/625/wiki_dump.txt", "r");
        for (lineCount = 0; lineCount < BATCH_SIZE; lineCount++)
        {
-           lines[i] = (char*)malloc(MAX_STRING);
+           lines[lineCount] = (char*)malloc(sizeof(char) * MAX_STRING);
            err = fscanf(fd, "%[^\n]\n", lines[lineCount]);
            if (err == EOF)
            {
@@ -92,13 +107,22 @@ int main(int argc, char* argv[])
            }
        }
        fclose( fd );
+       *localLineCount = lineCount;
    }
 
-   MPI_Bcast(lines, BATCH_SIZE * MAX_STRING, MPI_CHAR, 0, MPI_COMM_WORLD);
+   local_lines = malloc(sizeof(char**) * BATCH_SIZE);
+   for (i = 0; i < BATCH_SIZE; i++)
+   {
+       local_lines[i] = malloc(sizeof(char) * MAX_STRING);
+   }
+
+   local_averages = malloc(sizeof(float) * BATCH_SIZE);
+   MPI_Scatter(lines, BATCH_SIZE * MAX_STRING, MPI_CHAR, local_lines, BATCH_SIZE * MAX_STRING, MPI_CHAR, 0, MPI_COMM_WORLD);   
+   MPI_Bcast(localLineCount, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
    calculate_averages(&rank, lineCount);
 
-   MPI_Reduce(local_averages, global_averages, BATCH_SIZE, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
+   MPI_Gather(local_averages, BATCH_SIZE, MPI_FLOAT, global_averages, BATCH_SIZE, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
    if (rank == 0)
    {
