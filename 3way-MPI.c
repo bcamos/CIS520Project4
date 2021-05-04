dc3e03cf1aec58cf50492da48505e8f49610e730
Ben Amos <bcamos@ksu.edu>
2021-05-04 13:29:36 -0500

    MPI solution

--- 3way-MPI.c
+++ 3way-MPI.c
@@ -5,8 +5,8 @@
 
 #define MAX_SIZE 1000000
 #define MAX_STRING 2001
-char* lines[MAX_SIZE];
-char** local_lines;
+char lines[MAX_SIZE][MAX_STRING];
+char* local_lines;
 float* global_averages;
 float* local_averages;
 int NUM_THREADS;
@@ -37,35 +37,17 @@ void calculate_averages(void *rank, int lastIdx)
     int i;
     int strLength;
     int myId = *((int*) rank);
-    int startPos = ((long)myId) * (BATCH_SIZE / NUM_THREADS);
-    int endPos = startPos + (BATCH_SIZE / NUM_THREADS);
-    if (endPos - startPos < BATCH_SIZE)
-    {
-        endPos -= startPos;
-    }
-    else
-    {
-        endPos = BATCH_SIZE;
-    }
-
-    printf("myID = %d startPos = %d endPos = %d \n", myId, startPos, endPos); fflush(stdout);
 
     for (i = 0; i < BATCH_SIZE; i++)
     {
         local_averages[i] = 0.0f;
     }
-
-    printf("myID = %d Init success \n", myId); fflush(stdout);
-
-    if (myId == 0)
+    
+    for (i = 0; i < BATCH_SIZE; i++)
     {
-        for (i = 0; i < endPos; i++)
-        {
-            strLength = strlen(local_lines[i]);
-            local_averages[i] = find_avg(local_lines[i], strLength);
-        }
-    }    
-    printf("myID = %d Done! \n", myId); fflush(stdout);
+        strLength = strlen(local_lines + (i * MAX_STRING));
+        local_averages[i] = find_avg(local_lines + (i * MAX_STRING), strLength);
+    }
 }
 
 int main(int argc, char* argv[])
@@ -97,9 +79,8 @@ int main(int argc, char* argv[])
    {
        global_averages = malloc(sizeof(float) * MAX_SIZE);
        fd = fopen("/homes/dan/625/wiki_dump.txt", "r");
-       for (lineCount = 0; lineCount < BATCH_SIZE; lineCount++)
+       for (lineCount = 0; lineCount < MAX_SIZE; lineCount++)
        {
-           lines[lineCount] = (char*)malloc(sizeof(char) * MAX_STRING);
            err = fscanf(fd, "%[^\n]\n", lines[lineCount]);
            if (err == EOF)
            {
@@ -110,11 +91,7 @@ int main(int argc, char* argv[])
        *localLineCount = lineCount;
    }
 
-   local_lines = malloc(sizeof(char**) * BATCH_SIZE);
-   for (i = 0; i < BATCH_SIZE; i++)
-   {
-       local_lines[i] = malloc(sizeof(char) * MAX_STRING);
-   }
+   local_lines = malloc(sizeof(char) * BATCH_SIZE * MAX_STRING);
 
    local_averages = malloc(sizeof(float) * BATCH_SIZE);
    MPI_Scatter(lines, BATCH_SIZE * MAX_STRING, MPI_CHAR, local_lines, BATCH_SIZE * MAX_STRING, MPI_CHAR, 0, MPI_COMM_WORLD);   
