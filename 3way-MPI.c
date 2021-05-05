ece4a30c2f77801e48cf0ef42d241131ef7cef6b
Ben Amos <bcamos@ksu.edu>
2021-05-04 19:55:40 -0500

    Mpi solution with time

--- 3way-MPI.c
+++ 3way-MPI.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/time.h>
 
 #define MAX_SIZE 1000000
 #define MAX_STRING 2001
@@ -50,16 +51,30 @@ void calculate_averages(void *rank, int lastIdx)
     }
 }
 
+void calculate_averages_serial(int length)
+{
+    int i;
+    int strLength;
+    for ( i = 0; i < length; i++ )
+    {
+        strLength = strlen(lines[i]);
+        global_averages[i] = find_avg(lines[i], strLength);
+    }
+}
+
 int main(int argc, char* argv[])
 {
+   int display = 0;
    int maxlines = 1000000;
    int i, lineCount, err;
    int numtasks, rank;
-   float  charsum=0.0; 
+   float charsum = 0.0; 
    int nchars = 0; 
+   struct timeval t1, t2;
+   double elapsedTime;
    FILE *fd;
    MPI_Status Status;
-
+   gettimeofday(&t1, NULL);
    err = MPI_Init(&argc, &argv);
    if (err != MPI_SUCCESS) 
    {
@@ -72,7 +87,7 @@ int main(int argc, char* argv[])
    
    NUM_THREADS = numtasks;
    BATCH_SIZE = MAX_SIZE / NUM_THREADS;
-   printf("size = %d rank = %d\n", numtasks, rank);
+
    fflush(stdout);
    int* localLineCount = malloc(sizeof(int));
    if (rank == 0)
@@ -91,25 +106,42 @@ int main(int argc, char* argv[])
        *localLineCount = lineCount;
    }
 
-   local_lines = malloc(sizeof(char) * BATCH_SIZE * MAX_STRING);
+   if (NUM_THREADS > 1)
+   {
+       local_lines = malloc(sizeof(char) * BATCH_SIZE * MAX_STRING);
 
-   local_averages = malloc(sizeof(float) * BATCH_SIZE);
-   MPI_Scatter(lines, BATCH_SIZE * MAX_STRING, MPI_CHAR, local_lines, BATCH_SIZE * MAX_STRING, MPI_CHAR, 0, MPI_COMM_WORLD);   
-   MPI_Bcast(localLineCount, 1, MPI_INT, 0, MPI_COMM_WORLD);
+       local_averages = malloc(sizeof(float) * BATCH_SIZE);
+       MPI_Scatter(lines, BATCH_SIZE * MAX_STRING, MPI_CHAR, local_lines, BATCH_SIZE * MAX_STRING, MPI_CHAR, 0, MPI_COMM_WORLD);
+       MPI_Bcast(localLineCount, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-   calculate_averages(&rank, lineCount);
+       calculate_averages(&rank, lineCount);
 
-   MPI_Gather(local_averages, BATCH_SIZE, MPI_FLOAT, global_averages, BATCH_SIZE, MPI_FLOAT, 0, MPI_COMM_WORLD);
+       MPI_Gather(local_averages, BATCH_SIZE, MPI_FLOAT, global_averages, BATCH_SIZE, MPI_FLOAT, 0, MPI_COMM_WORLD);
+   }
+   else
+   {
+       calculate_averages_serial(lineCount);
+   }
 
-   if (rank == 0)
+
+   if (rank == 0 && display != 0)
    {
        for (i = 0; i < lineCount; i++)
        {
            printf("%d: %.1f\n", i, global_averages[i]);
-       }       
+       }     
    }
 
    MPI_Finalize();
+
+   if (rank == 0)
+   {
+       gettimeofday(&t2, NULL);
+
+       elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;
+       elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;
+       printf("DATA, %d, %f\n", NUM_THREADS, elapsedTime);
+   }
    return 0;
 }
 
